66a602d9d0c60a7b0f8ecf0d506b013c5e54271b
yuehao <879389355@qq.com>
2021-02-10 21:58:55 +0800

    feat：添加多种输入的情况

--- week/change.c
+++ week/change.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-  const int AMOUNT = 100;
+  int amount = 0;
   int price = 0;
 
   printf("请输入金额（元）：");
-  scanf("%d", &price);
+  scanf("%d %d", &price, &amount);
 
-  int change = AMOUNT - price;
+  int change = amount - price;
 
   printf("找您%d元。 \n", change);
 
